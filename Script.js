db43f286038a8dcd9c7098bff5c6578eac545c20
Anh Phong VIRASSAMY <anhphong.virassamy@gmail.com>
2021-04-04 01:30:54 +0200

    présentation

--- Script.js
+++ Script.js
@@ -1,10 +1,11 @@
 var nav = document.querySelector('nav');
 window.addEventListener('scroll', function () {
-    if(window.pageYOffset > 100){
-        nav.classList.add('bg-dark', 'shadow');
+    if(window.pageYOffset > 200){
+        nav.classList.add('bg-secondary', 'shadow');
+        nav.classList.replace('navbar-light','navbar-dark');
     }
     else {
-        nav.classList.remove('bg-dark','shadow');
+        nav.classList.remove('bg-secondary','shadow');
+        nav.classList.replace('navbar-dark','navbar-light');
     }
 });
-
